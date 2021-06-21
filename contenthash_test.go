a9020c42e3d7ab2804aa6f308171a566daa59e52
Jim McDonald <Jim@mcdee.net>
2021-06-21 15:02:29 +0100

    Updates for current multiformats.

--- contenthash_test.go
+++ contenthash_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Weald Technology Trading
+// Copyright 2019-201 Weald Technology Trading
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,88 +15,134 @@
 package ens
 
 import (
+	"encoding/hex"
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func _hexStr(input string) []byte {
+	res, err := hex.DecodeString(input)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
 
 func TestContenthash(t *testing.T) {
 	tests := []struct {
+		name string
 		repr string
 		bin  []byte
+		res  string
 		err  error
 	}{
-		{ // 0
+		{
+			name: "Nil",
 			repr: "",
-			err:  errors.New("invalid content hash"),
+			err:  errors.New("no content hash"),
 		},
-		{ // 1
-			repr: "/bad/",
+		{
+			name: "CodecMissing",
+			repr: "://QmayQq2DWCkY3d4x3xKh4suohuRPEXe2fBqMBam5xtDj3t",
+			err:  errors.New("codec missing"),
+		},
+		{
+			name: "CodecMissing2",
+			repr: "//QmayQq2DWCkY3d4x3xKh4suohuRPEXe2fBqMBam5xtDj3t",
+			err:  errors.New("codec missing"),
+		},
+		{
+			name: "Bad",
+			repr: "/bad/QmayQq2DWCkY3d4x3xKh4suohuRPEXe2fBqMBam5xtDj3t",
 			err:  errors.New("unknown codec bad"),
 		},
-		{ // 2
+		{
+			name: "MissingData",
 			repr: "/ipfs/",
-			err:  errors.New("failed to obtain IPFS hash"),
+			err:  errors.New("data missing"),
 		},
-		{ // 3
-			repr: "/ipfs/bad",
-			err:  errors.New("failed to obtain IPFS hash"),
+		{
+			name: "BadHash",
+			repr: "/ipfs/invalid",
+			err:  errors.New("invalid IPFS data: selected encoding not supported"),
 		},
-		{ // 4
-			repr: "/ipfs/QmayQq2DWCkY3d4x3xKh4suohuRPEXe2fBqMBam5xtDj3t",
-			bin:  []byte{0xe3, 0x1, 0x1, 0x70, 0x12, 0x20, 0xbb, 0xb7, 0x9, 0xbb, 0x1c, 0x1f, 0xd9, 0x10, 0x7d, 0x81, 0x8, 0x3f, 0x36, 0xe3, 0x6d, 0xd9, 0x71, 0xe4, 0xfb, 0xfe, 0x45, 0x77, 0xa8, 0x6a, 0x53, 0xd1, 0x91, 0xe, 0x3b, 0xcd, 0xbd, 0xff},
+		{
+			name: "IPFSCIDv0",
+			repr: "/ipfs/QmRAQB6YaCyidP37UdDnjFY5vQuiBrcqdyoW1CuDgwxkD4",
+			bin:  _hexStr("e3010170122029f2d17be6139079dc48696d1f582a8530eb9805b561eda517e22a892c7e3f1f"),
+			res:  "ipfs://k2jmtxseqz46solsx2rmxavgbzp6ij1t1kiq1or8a00c2g9bx1for0gv",
 		},
-		{ // 5
-			repr: "/ipns/QmQ4QZh8nrsczdUEwTyfBope4THUhqxqc1fx6qYhhzZQei",
-			bin:  []byte{0xe5, 0x1, 0x1, 0x70, 0x12, 0x20, 0x19, 0x8e, 0x16, 0x47, 0x97, 0x81, 0xa5, 0x73, 0xfc, 0x91, 0x42, 0xc1, 0xf1, 0x48, 0x7d, 0x67, 0xd0, 0x2c, 0xdc, 0x63, 0xd1, 0x19, 0x97, 0xb1, 0xb9, 0x9f, 0xf8, 0x85, 0x4e, 0x65, 0x22, 0xd7},
+		{
+			name: "IPFSCIDv1",
+			repr: "/ipfs/bafybeibj6lixxzqtsb45ysdjnupvqkufgdvzqbnvmhw2kf7cfkesy7r7d4",
+			bin:  _hexStr("e3010170122029f2d17be6139079dc48696d1f582a8530eb9805b561eda517e22a892c7e3f1f"),
+			res:  "ipfs://k2jmtxseqz46solsx2rmxavgbzp6ij1t1kiq1or8a00c2g9bx1for0gv",
 		},
-		{ // 6
-			repr: "/ipns/wealdtech.eth",
-			bin:  []byte{0xe5, 0x1, 0x1, 0x70, 0x0, 0x0d, 0x77, 0x65, 0x61, 0x6c, 0x64, 0x74, 0x65, 0x63, 0x68, 0x2e, 0x65, 0x74, 0x68},
+		{
+			name: "Onion",
+			repr: "/onion/zqktlwi4fecvo6ri",
+			bin:  _hexStr("bc037a716b746c776934666563766f367269"),
+			res:  "onion://zqktlwi4fecvo6ri",
 		},
-		{ // 7
-			repr: "/swarm",
-			err:  errors.New("invalid content hash"),
+		{
+			name: "Onion3",
+			repr: "/onion3/p53lf57qovyuvwsc6xnrppyply3vtqm7l6pcobkmyqsiofyeznfu5uqd",
+			bin:  _hexStr("bd037035336c663537716f7679757677736336786e72707079706c79337674716d376c3670636f626b6d797173696f6679657a6e667535757164"),
+			res:  "onion3://p53lf57qovyuvwsc6xnrppyply3vtqm7l6pcobkmyqsiofyeznfu5uqd",
 		},
-		{ // 8
-			repr: "/swarm/bad",
-			err:  errors.New("failed to decode swarm content hash"),
+		{
+			name: "IPFS",
+			repr: "ipfs://QmRAQB6YaCyidP37UdDnjFY5vQuiBrcqdyoW1CuDgwxkD4",
+			bin:  _hexStr("e3010170122029f2d17be6139079dc48696d1f582a8530eb9805b561eda517e22a892c7e3f1f"),
+			res:  "ipfs://k2jmtxseqz46solsx2rmxavgbzp6ij1t1kiq1or8a00c2g9bx1for0gv",
 		},
-		{ // 9
+		{
+			name: "IPNSIdentity",
+			repr: "/ipns/k51qzi5uqu5djwbl0zcd4g9onue26a8nq97c0m9wp6kir1gibuyjxpkqpoxwag",
+			bin:  _hexStr("e5010172002408011220950b8f62b925ecc50247cc8de1084b43f854fbc452894d9a1a97d7f27d0addb8"),
+			res:  "ipns://k51qzi5uqu5djwbl0zcd4g9onue26a8nq97c0m9wp6kir1gibuyjxpkqpoxwag",
+		},
+		{
+			name: "IPNSEIP1577",
+			repr: "ipns://k51qzi5uqu5djwbl0zcd4g9onue26a8nq97c0m9wp6kir1gibuyjxpkqpoxwag",
+			bin:  _hexStr("e5010172002408011220950b8f62b925ecc50247cc8de1084b43f854fbc452894d9a1a97d7f27d0addb8"),
+			res:  "ipns://k51qzi5uqu5djwbl0zcd4g9onue26a8nq97c0m9wp6kir1gibuyjxpkqpoxwag",
+		},
+		{
+			name: "SwarmBad",
+			repr: "/swarm/invalid",
+			err:  errors.New("invalid hex: encoding/hex: invalid byte: U+0069 'i'"),
+		},
+		{
+			name: "Swarm",
 			repr: "/swarm/d1de9994b4d039f6548d191eb26786769f580809256b4685ef316805265ea162",
-			bin:  []byte{0xe4, 0x1, 0x1, 0xfa, 0x1, 0x1b, 0x20, 0xd1, 0xde, 0x99, 0x94, 0xb4, 0xd0, 0x39, 0xf6, 0x54, 0x8d, 0x19, 0x1e, 0xb2, 0x67, 0x86, 0x76, 0x9f, 0x58, 0x8, 0x9, 0x25, 0x6b, 0x46, 0x85, 0xef, 0x31, 0x68, 0x5, 0x26, 0x5e, 0xa1, 0x62},
+			bin:  _hexStr("e40101fa011b20d1de9994b4d039f6548d191eb26786769f580809256b4685ef316805265ea162"),
+			res:  "bzz://d1de9994b4d039f6548d191eb26786769f580809256b4685ef316805265ea162",
 		},
-		{ // 10
-			repr: "/onion/",
-			err:  errors.New("onion address should be 16 characters"),
-		},
-		{ // 11
-			repr: "/onion/3g2upl4pq6kufc4m",
-			bin:  []byte{0xbc, 0x03, 0x33, 0x67, 0x32, 0x75, 0x70, 0x6c, 0x34, 0x70, 0x71, 0x36, 0x6b, 0x75, 0x66, 0x63, 0x34, 0x6d},
-		},
-		{ // 12
-			repr: "/onion3/",
-			err:  errors.New("onion3 address should be 56 characters"),
-		},
-		{ // 13
-			repr: "/onion3/4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnzjpbi7utijcltosqemad",
-			bin:  []byte{0xbd, 0x03, 0x34, 0x61, 0x63, 0x74, 0x68, 0x34, 0x37, 0x69, 0x36, 0x6b, 0x78, 0x6e, 0x76, 0x6b, 0x65, 0x77, 0x74, 0x6d, 0x36, 0x71, 0x37, 0x69, 0x62, 0x32, 0x73, 0x33, 0x75, 0x66, 0x70, 0x6f, 0x35, 0x73, 0x71, 0x62, 0x73, 0x6e, 0x7a, 0x6a, 0x70, 0x62, 0x69, 0x37, 0x75, 0x74, 0x69, 0x6a, 0x63, 0x6c, 0x74, 0x6f, 0x73, 0x71, 0x65, 0x6d, 0x61, 0x64},
+		{
+			name: "bzz",
+			repr: "bzz://d1de9994b4d039f6548d191eb26786769f580809256b4685ef316805265ea162",
+			bin:  []byte{0xe4, 0x01, 0x01, 0xfa, 0x01, 0x1b, 0x20, 0xd1, 0xde, 0x99, 0x94, 0xb4, 0xd0, 0x39, 0xf6, 0x54, 0x8d, 0x19, 0x1e, 0xb2, 0x67, 0x86, 0x76, 0x9f, 0x58, 0x08, 0x09, 0x25, 0x6b, 0x46, 0x85, 0xef, 0x31, 0x68, 0x05, 0x26, 0x5e, 0xa1, 0x62},
+			res:  "bzz://d1de9994b4d039f6548d191eb26786769f580809256b4685ef316805265ea162",
 		},
 	}
 
-	for i, test := range tests {
-		bin, err := StringToContenthash(test.repr)
-		if test.err != nil {
-			assert.NotNil(t, err, fmt.Sprintf("missing error at test %d", i))
-			assert.Equal(t, test.err.Error(), err.Error(), fmt.Sprintf("incorrect error at test %d", i))
-		} else {
-			assert.Nil(t, err, fmt.Sprintf("unexpected error creating content hash at test %d", i))
-			assert.Equal(t, test.bin, bin, fmt.Sprintf("incorrect coding at test %d", i))
-			repr, err := ContenthashToString(bin)
-			assert.Nil(t, err, fmt.Sprintf("unexpected error decoding content hash at test %d", i))
-			assert.Equal(t, test.repr, repr, fmt.Sprintf("incorrect decoding at test %d", i))
-		}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			bin, err := StringToContenthash(test.repr)
+			if test.err != nil {
+				require.NotNil(t, err, "missing error")
+				assert.Equal(t, test.err.Error(), err.Error(), "incorrect error")
+			} else {
+				require.Nil(t, err, "unexpected error creating content hash")
+				require.Equal(t, test.bin, bin, "incorrect coding")
+				res, err := ContenthashToString(bin)
+				require.Nil(t, err, "unexpected error decoding content hash")
+				assert.Equal(t, test.res, res, "incorrect decoding")
+			}
+		})
 	}
 }
